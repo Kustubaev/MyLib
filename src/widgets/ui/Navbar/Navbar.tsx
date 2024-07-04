78428d696f6f76177dfcd528af2e98ea9fe80512
Павел Зозин <haniemcs@gmail.com>
2024-07-05 02:39:13 +0500

    Ya durak nakosyachil. Reshau problemi

--- src/widgets/ui/Navbar/Navbar.tsx
+++ src/widgets/ui/Navbar/Navbar.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { NavButton } from 'src/shared/ui/NavButton/NavButton';
+import cls from './Navbar.module.scss';
 
 export const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className={cls.navbar}>
+      <ul className={cls.navbar__ul}>
+        <li className={cls.navbar__ul__li}>
           <NavButton href='/'>Главная</NavButton>
         </li>
-        <li>
+        <li className={cls.navbar__ul__li}>
           <NavButton href='books'>Книги</NavButton>
         </li>
-        <li>
+        <li className={cls.navbar__ul__li}>
           <NavButton href='book'>Книга</NavButton>
         </li>
-        <li>
+        <li className={cls.navbar__ul__li}>
           <NavButton href='history'>История</NavButton>
         </li>
-        <li>
+        <li className={cls.navbar__ul__li}>
           <NavButton href='login'>Вход</NavButton>
         </li>
       </ul>
